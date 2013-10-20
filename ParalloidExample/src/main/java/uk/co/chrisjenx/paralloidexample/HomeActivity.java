d49b038b2ab31171b137718dd38a00984d223317
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2013-10-20 15:26:51 +0100

    seperated out the example

--- ParalloidExample/src/main/java/uk/co/chrisjenx/paralloidexample/HomeActivity.java
+++ ParalloidExample/src/main/java/uk/co/chrisjenx/paralloidexample/HomeActivity.java
@@ -5,17 +5,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ScrollView;
 
 import java.util.Locale;
-
-import uk.co.chrisjenx.paralloid.Parallaxor;
 
 public class HomeActivity extends FragmentActivity {
 
@@ -56,14 +50,13 @@ public class HomeActivity extends FragmentActivity {
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
-    
-    
+
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -101,33 +94,4 @@ public class HomeActivity extends FragmentActivity {
             return null;
         }
     }
-
-    /**
-     * A dummy fragment representing a section of the app, but that simply
-     * displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        public DummySectionFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home_dummy, container, false);
-            ImageView imageView = (ImageView) rootView.findViewById(R.id.image_view);
-            ScrollView scrollView = (ScrollView) rootView.findViewById(R.id.scroll_view);
-            if (scrollView instanceof Parallaxor) {
-                ((Parallaxor) scrollView).parallaxViewBy(imageView, 0.5f);
-            }
-
-            return rootView;
-        }
-    }
-
 }
